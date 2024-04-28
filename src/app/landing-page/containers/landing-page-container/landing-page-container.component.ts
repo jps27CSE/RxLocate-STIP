a43c1d44a6a5685b48b76648dc02285a0f444bb8
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-04-28 17:53:49 +0600

    feature banner

--- src/app/landing-page/containers/landing-page-container/landing-page-container.component.ts
+++ src/app/landing-page/containers/landing-page-container/landing-page-container.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import {BannerComponent} from "../../views/banner/banner.component";
+import { BannerComponent } from '../../views/banner/banner.component';
+import { FeaturesComponent } from '../../views/features/features.component';
 
 @Component({
   selector: 'app-landing-page-container',
   standalone: true,
-  imports: [
-    BannerComponent
-  ],
+  imports: [BannerComponent, FeaturesComponent],
   templateUrl: './landing-page-container.component.html',
-  styleUrl: './landing-page-container.component.css'
+  styleUrl: './landing-page-container.component.css',
 })
-export class LandingPageContainerComponent {
-
-}
+export class LandingPageContainerComponent {}
