e7fc208b6e6e4a6a113c95e99888fe947d6823be
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-04-29 11:07:49 +0600

    Footer and 404 Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from "./common/navbar/navbar.component";
-import {NgOptimizedImage} from "@angular/common";
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { NgOptimizedImage } from '@angular/common';
+import { FooterComponent } from './common/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, NgOptimizedImage],
+  imports: [RouterOutlet, NavbarComponent, NgOptimizedImage, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'RxLocate-STIP';
