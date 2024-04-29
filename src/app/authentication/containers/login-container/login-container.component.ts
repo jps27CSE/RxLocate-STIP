b4f96724079d3f8beab62da3228d62c8cbe87bc8
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-04-29 12:50:03 +0600

    Login Page Folder Structure

---
+++ src/app/authentication/containers/login-container/login-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from '../../views/login/login.component';
+
+@Component({
+  selector: 'app-login-container',
+  standalone: true,
+  imports: [LoginComponent],
+  templateUrl: './login-container.component.html',
+  styleUrl: './login-container.component.css',
+})
+export class LoginContainerComponent {}
