34269a70e8b05fabdfd41f4a7fe77a6fc3b78a92
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-04-29 11:36:18 +0600

    FAQ Section

--- src/app/landing-page/containers/landing-page-container/landing-page-container.component.ts
+++ src/app/landing-page/containers/landing-page-container/landing-page-container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../views/banner/banner.component';
 import { FeaturesComponent } from '../../views/features/features.component';
+import { FaqSectionComponent } from '../../views/faq-section/faq-section.component';
 
 @Component({
   selector: 'app-landing-page-container',
   standalone: true,
-  imports: [BannerComponent, FeaturesComponent],
+  imports: [BannerComponent, FeaturesComponent, FaqSectionComponent],
   templateUrl: './landing-page-container.component.html',
   styleUrl: './landing-page-container.component.css',
 })
