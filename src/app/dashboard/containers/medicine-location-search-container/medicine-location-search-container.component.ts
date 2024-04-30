c1c53e26323db8f20da6a5ce663ccea4c1885703
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-04-30 16:20:39 +0600

    Search Form Input Data Functionality

--- src/app/dashboard/containers/medicine-location-search-container/medicine-location-search-container.component.ts
+++ src/app/dashboard/containers/medicine-location-search-container/medicine-location-search-container.component.ts
@@ -8,4 +8,9 @@ import { SearchFormComponent } from '../../views/search-form/search-form.compone
   templateUrl: './medicine-location-search-container.component.html',
   styleUrl: './medicine-location-search-container.component.css',
 })
-export class MedicineLocationSearchContainerComponent {}
+export class MedicineLocationSearchContainerComponent {
+  onSubmit(formData: { location: string; medicine: string }): void {
+    console.log('Location:', formData.location);
+    console.log('Medicine:', formData.medicine);
+  }
+}
--- src/app/dashboard/views/search-form/search-form.component.ts
+++ src/app/dashboard/views/search-form/search-form.component.ts
@@ -1,15 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 import { AnimationItem } from 'lottie-web';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-search-form',
   standalone: true,
-  imports: [LottieComponent],
+  imports: [LottieComponent, ReactiveFormsModule],
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.css',
 })
 export class SearchFormComponent {
+  @Output() formSubmit: EventEmitter<{ location: string; medicine: string }> =
+    new EventEmitter();
+  searchForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      location: ['', [Validators.required]],
+      medicine: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.searchForm.valid) {
+      const formData = this.searchForm.value;
+      this.formSubmit.emit(formData);
+    }
+  }
+
   options: AnimationOptions = {
     path: './assets/animation/search.json',
   };
@@ -20,4 +44,5 @@ export class SearchFormComponent {
     maxWidth: '70%',
     margin: '0 auto',
   };
+  protected readonly onsubmit = onsubmit;
 }
