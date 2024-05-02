ff39d91ceab7a36a476ebe5f849a75a81c81e820
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-05-02 11:25:54 +0600

    Map Details Component Design

---
+++ src/app/dashboard/views/map-details/map-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './map-details.component.html',
+  styleUrl: './map-details.component.css'
+})
+export class MapDetailsComponent {
+
+}
