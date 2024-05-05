ac44a56e01b38617a2f192fbbaea031806de4b22
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2024-05-05 11:52:10 +0600

    Attached Map on Dashboard/Map

--- src/app/dashboard/views/map-view/map-view.component.ts
+++ src/app/dashboard/views/map-view/map-view.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map-view',
   standalone: true,
   imports: [],
   templateUrl: './map-view.component.html',
-  styleUrl: './map-view.component.css'
+  styleUrl: './map-view.component.css',
 })
-export class MapViewComponent {
+export class MapViewComponent implements OnInit {
+  ngOnInit() {
+    this.configMap();
+  }
 
+  map: any;
+
+  configMap() {
+    this.map = L.map('map', {
+      center: [23.78049269183336, 90.40754216930443],
+      zoom: 6,
+    });
+
+    L.tileLayer(
+      'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png',
+      {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      },
+    ).addTo(this.map);
+
+    L.marker([23.78049269183336, 90.40754216930443])
+      .addTo(this.map)
+      .bindPopup('Square Health.')
+      .openPopup();
+  }
 }
